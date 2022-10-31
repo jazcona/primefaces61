d991de4ab82241d7684077f976ce600021058440
jazcona <javiazcona@gmail.com>
2022-10-31 14:27:20 +0100

    Pruebas dialog auto height

--- jsf-primefaces-apache-tomcat/src/main/java/com/codenotfound/primefaces/HelloWorld.java
+++ jsf-primefaces-apache-tomcat/src/main/java/com/codenotfound/primefaces/HelloWorld.java
@@ -1,12 +1,26 @@
 package com.codenotfound.primefaces;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class HelloWorld {
+  private CarService service;
 
   private String firstName = "John";
   private String lastName = "Doe";
+  private List<Car> cars;
+
+  public List<Car> getCars() {
+    service = new CarService();
+    cars = service.createCars(10);
+    return cars;
+  }
+
+  public void setCars(List<Car> cars) {
+    this.cars = cars;
+  }
 
   public String getFirstName() {
     return firstName;
